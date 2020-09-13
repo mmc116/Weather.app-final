4c2a25b66f70a96bccf1db2f02340e95c57d2861
mmc116 <mmc116@users.noreply.github.com>
2020-09-13 03:28:06 +0100

    made search functional
    created functions to:
    - get current location and display weather info;
    - display info of searched location;
    - get date and time

--- src/java.js
+++ src/java.js
@@ -1,4 +1,57 @@
-function showCityTemperature(response) {
+function showTime() {
+  let now = new Date();
+  let hourDate = document.querySelector("#hour-date");
+  let hour = now.getHours();
+  let minutes = now.getMinutes();
+  let weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+  let weekDay = weekDays[now.getDay()];
+  let monthDay = now.getDate();
+  let month = months[now.getMonth()];
+  let year = now.getFullYear();
+  hourDate.innerHTML = `${hour}:${minutes} ${weekDay} <br />${monthDay}.${month}.${year}`;
+}
+
+showTime();
+
+function showLocationWeather(response) {
+  let cityName = response.data.name;
+  let temperature = Math.round(response.data.main.temp);
+  let h1 = document.querySelector("h1");
+  let temp = document.querySelector("#temperature");
+  let wind = document.querySelector("#wind-speed");
+  let humidity = document.querySelector("#humidity");
+  let real = document.querySelector("#real-feel");
+  h1.innerHTML = `${cityName}`;
+  temp.innerHTML = `${temperature}°`;
+  wind.innerHTML = `Wind: ${Math.round(response.data.wind.speed)}km/h`;
+  humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  real.innerHTML = `Real Feel: ${Math.round(response.data.main.feels_like)}°c`;
+}
+function getLocationWeather(position) {
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+  let apiKey = "ff3fb90c5258ccf0229e6dfb9eb39e40";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showLocationWeather);
+}
+
+function getPosition() {
+  navigator.geolocation.getCurrentPosition(getLocationWeather);
+}
+
+getPosition();
+
+function searchedCityTemperature(response) {
   let city = document.querySelector("#city");
   let temp = document.querySelector("#temperature");
   let wind = document.querySelector("#wind-speed");
@@ -18,7 +71,7 @@ function getCityTemperature(event) {
   let apiKey = "ff3fb90c5258ccf0229e6dfb9eb39e40";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=${unit}`;
 
-  axios.get(apiUrl).then(showCityTemperature);
+  axios.get(apiUrl).then(searchedCityTemperature);
 }
 
 let searchCity = document.querySelector("#search-button");
